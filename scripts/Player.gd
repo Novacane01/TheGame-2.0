a069a7dc016b5bfdf3dd60fcfa7f7c2d949ae2d7
Kayton Fletcher <kaytonfletcher@gmail.com>
2021-11-01 21:19:57 -0400

    modifies movement slightly, adds health bar and tree scene

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,37 +1,33 @@
 extends KinematicBody2D
 
+const ACCELERATION: float = 800.0
+const MAX_SPEED: float = 100.0
+const FRICTION: float = 800.0
+const GRAVITY: float = 600.0
+const JUMP_POWER: float = 200.0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-const ACCELERATION: float = 10.0;
-const MAX_SPEED: float = 100.0;
-const FRICTION: float = 10.0;
-const GRAVITY: float = 5.0;
-const JUMP_POWER: float = 100.0;
+var velocity: Vector2 = Vector2.ZERO
 
-var velocity: Vector2 = Vector2.ZERO;
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	print("Ready!");
-	print(owner);
-	pass # Replace with function body.
+	print("Ready!")
 
-func _physics_process(_delta):
-	var target_movement: Vector2 = Vector2.ZERO;
-	target_movement.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
+
+func _process(delta):
+	var target_movement: Vector2 = Vector2.ZERO
+	target_movement.x = (
+		Input.get_action_strength("move_right")
+		- Input.get_action_strength("move_left")
+	)
 	if is_on_floor():
-		target_movement.y = -Input.get_action_strength("jump");
-		velocity.y = target_movement.y * JUMP_POWER;
+		target_movement.y = -Input.get_action_strength("jump")
+		velocity.y = target_movement.y * JUMP_POWER
 	if target_movement != Vector2.ZERO:
-		velocity = velocity.move_toward(Vector2(target_movement.x * MAX_SPEED, velocity.y), ACCELERATION);
+		velocity = velocity.move_toward(Vector2(target_movement.x * MAX_SPEED, velocity.y), ACCELERATION * delta)
 	else:
-		velocity = velocity.move_toward(Vector2(0.0, velocity.y), FRICTION)
+		velocity = velocity.move_toward(Vector2(0.0, velocity.y), FRICTION * delta)
 
 	# Simulate real gravity; constant force being applied
-	velocity.y += GRAVITY;
-	velocity = move_and_slide(velocity, Vector2(0.0,-1.0), true);
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	velocity.y += GRAVITY * delta
+	
+	velocity = move_and_slide(velocity, Vector2(0.0, -1.0))
